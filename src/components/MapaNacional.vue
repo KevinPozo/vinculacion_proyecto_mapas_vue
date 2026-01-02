daaed136731591e1e18cf95804b2be0a0b9e68b2
BelenPaca <bapaca@uce.edu.ec>
2026-01-01 23:09:42 -0500

    GeoJSON nacional y estructura de vista de resultados

---
+++ src/components/MapaNacional.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <MapaNacional /> 
+  </div>
+</template>
+
+<script>
+
+import MapaNacional from "@/components/MapaNacional.vue";
+
+export default {
+  name: "PaginaResultadosExtranjero",
+  components: {
+    MapaNacional,
+  },
+};
+</script>
--- src/views/PaginaResultadosExtranjero.vue
+++ src/views/PaginaResultadosExtranjero.vue
@@ -1,18 +1,22 @@
 <!-- src/views/PaginaResultadosExtranjero.vue -->
 <template>
   <div>
-    <MapaExtranjero />
+    <!--MapaExtranjero /-->
+    <MapaNacional />
+    
     
   </div>
 </template>
 
 <script>
 import MapaExtranjero from "@/components/MapaExtranjero.vue";
+import MapaNacional from "@/components/MapaNacional.vue";
 
 export default {
   name: "PaginaResultadosExtranjero",
   components: {
     MapaExtranjero,
+    MapaNacional,
   },
 };
 </script>
